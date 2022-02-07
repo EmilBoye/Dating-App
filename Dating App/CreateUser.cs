c015ec0d4cc302df6f474c7c291c01082d749f7b
EmilBoye <70256366+EmilBoye@users.noreply.github.com>
2022-02-07 14:54:10 +0100

    Added some more to CreateUser
    Added CreateUserInfo

--- Dating App/CreateUser.cs
+++ Dating App/CreateUser.cs
@@ -10,16 +10,24 @@ namespace Dating_App
     {
         public string firstName { get; set; }
         public string lastName { get; set; }
+        public int Age { get; set; }
         public string gender { get; set; }
         public DateTime BirthDate { get; set; }
         public void CreateUserPage()
         {
             Console.Title = "Create Profile";
 
-            Console.WriteLine("Velkommen til oprettelse af bruger.");
+            Console.WriteLine("Velkommen til oprettelse af bruger.\n");
 
+            var info = new CreateUser();
+            info.CreateUserInfo();
+        }
 
-            Console.Write("");
+        public void CreateUserInfo()
+        {
+            Console.Write("Indtast fornavn: ");
+            firstName = Console.ReadLine();
+
         }
     }
 }
