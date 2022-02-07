8fa37abad0ae719164dbaa60e9ac3d1e4b1c9db5
EmilBoye <70256366+EmilBoye@users.noreply.github.com>
2022-02-07 13:28:29 +0100

    Update CreateUser

--- Dating App/CreateUser.cs
+++ Dating App/CreateUser.cs
@@ -8,9 +8,16 @@ namespace Dating_App
 {
     public class CreateUser
     {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
         public void CreateUserPage()
         {
-            Console.WriteLine("jf");
+            Console.Title = "Create Profile";
+
+            Console.WriteLine("Velkommen til oprettelse af bruger.");
+
+            Console.Write("");
         }
     }
 }
